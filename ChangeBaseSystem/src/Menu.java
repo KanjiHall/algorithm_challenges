a8e803f488727850050f15dff9d9295145f6e5bd
Kanji Hall <kanjihall1609@gmail.com>
2023-01-06 17:02:20 +0700

    For

--- ChangeBaseSystem/src/Menu.java
+++ ChangeBaseSystem/src/Menu.java
@@ -1,21 +1,4 @@
 public class Menu {
-    // Design a program that allows users choose the input base system (2, 10, 16)
-    // and the output base system (2,10,16) then enter input value, the program will
-    // print the equivalent output value
-    // The program should be repetitive until users close the program
-    // Function details:
-    // 1. Required user choose the base number input ( example 1 is binary, 2 is
-    // decimal, 3 is hexadecimal)
-    // 2. Required user choose the base number out ( example 1 is binary, 2 is
-    // decimal, 3 is hexadecimal)
-    // 3. Required user enter the input value;
-    // 4. Program process and print output value;
-    // 5. Program ask user if they want to continue or not;
-    // 6. If user choose to continue, program will repeat from step 1;
-    // 7. If user choose to exit, program will exit;
-    // 8. Program should be repetitive until users close the program
-    // 9. Program should be able to handle invalid input from user from Validation
-    // class
     public void Console() {
         do {
             // Clear screen after each loop
